64471ea059db485fc021a226072c11b9d2f20b71
d0peCode <tyminski.borys@gmail.com>
2021-12-24 15:21:08 +0100

    add small nav for poc

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul>
+      <li><NuxtLink to="/users">Users</NuxtLink></li>
+      <li><NuxtLink to="/groups">Groups</NuxtLink></li>
+      <li><NuxtLink to="/users/recent">Recent User</NuxtLink></li>
+    </ul>
+    <NuxtPage />
+  </div>
+</template>
